1bdda13d20cc38d11b1f754b5f21ae1f239f2263
zerospeedzero <zerospeedzero@gmail.com>
2022-12-16 16:11:19 -0700

    Fix checkout problem

--- checkout.php
+++ checkout.php
@@ -6,10 +6,10 @@
   foreach ($_SESSION["cart"] as $id => $quantity) {
     $products = $id . ',' . $products;
   }
-  echo "insert into orders (customer, email, products, price) VALUES ('" . $_POST['customer'] . "','" . $_POST['email'] . "','" . $products . "','" . $_POST['total'] . "')";
+  // echo "insert into orders (customer, email, products, price) VALUES ('" . $_POST['customer'] . "','" . $_POST['email'] . "','" . $products . "','" . $_POST['total'] . "')";
   $r = mysqli_query($conn, "insert into orders (customer, email, products, price) VALUES ('" . $_POST['customer'] . "','" . $_POST['email'] . "','" . $products . "','" . $_POST['total'] . "')");
-  var_dump($r);
-  echo mysqli_error($conn);
+  // var_dump($r);
+  // echo mysqli_error($conn);
   unset($_SESSION["cart"]);
   header("Location: ./index.php");
 ?>
